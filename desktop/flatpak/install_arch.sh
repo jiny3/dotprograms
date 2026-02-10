9adef61f7ca38a16edab3a3876e5a91adc256c16
jiny3 <101464224+jiny3@users.noreply.github.com>
2026-02-10 22:39:40 +0800

    add flathub remote-add

--- desktop/flatpak/install_arch.sh
+++ desktop/flatpak/install_arch.sh
@@ -3,5 +3,4 @@
 paru -S --noconfirm --needed flatpak
 
 # 添加 Flathub 仓库（如果尚未添加）
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-
+flatpak -u remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
